3c7b2e2464e9507b01a4198f15d776d3fa893351
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-06-05 14:16:24 +0300

    done exercise

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -12,36 +12,30 @@ const onInput = event => {
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
-console.log(onInput);
-
 const onSubmit = event => {
   event.preventDefault();
-  formData.email === '' || formData.message === ''
-    ? alert('Fill please all fields')
-    : console.log(formData);
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    refs.form.reset();
+    formData.email = '';
+    formData.message = '';
+  }
+};
+
+const fillFormFields = () => {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedData) {
+    refs.form.elements.email.value = savedData.email;
+    refs.form.elements.message.value = savedData.message;
+    formData.email = savedData.email;
+    formData.message = savedData.message;
+  }
 };
 
 refs.form.addEventListener('input', onInput);
 refs.form.addEventListener('submit', onSubmit);
 
-formData.email = refs.form.elements.email.value;
-formData.message = refs.form.elements.message.value;
-
-const fillFormFields = () => {
-  const savedData = localStorage.getItem('feedback-form-state');
-  if (savedData) {
-    const data = JSON.parse(savedData);
-    refs.form.elements.email.value = data.email;
-    refs.form.elements.message.value = data.message;
-  }
-};
 fillFormFields();
-
-// const formDataValues = Object.values(formData);
-
-// if (formDataValues.includes('')) {
-// } else {
-//   console.log(formData);
-//   localStorage.removeItem('feedback-form-state');
-//   refs.form.reset();
-// }
